c92b26e1bd91e09eface3398c8f3a1ee623f3853
Dashstrom <incerti.felix@gmail.com>
2021-06-25 13:21:04 +0200

    remove echo off in compile.bat

--- compile.bat
+++ compile.bat
@@ -1,10 +1,8 @@
 py setup.py build_ext --inplace
-: @echo off
 if "%ERRORLEVEL%" NEQ "0" (exit /B)
 
 MOVE /Y "fractale.cp39-win_amd64.pyd" "mandelbrot\model"
 DEL "mandelbrot\model\fractale.cpp"
 RMDIR "build" /s /q
-@echo on
 py main.py
 : pyinstaller --add-data "logo.ico";"." --noconsole --onefile --name=Mandelbrot --icon=logo.ico main.py
